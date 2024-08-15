78dc97880a0a7546d6baca70de52c66bec09642a
Pallav Jain <jainpallav61@gmail.com>
2024-08-16 00:41:28 +0530

    Port change

--- app.py
+++ app.py
@@ -200,5 +200,6 @@ def decode_route():
     
     return render_template('decode.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
